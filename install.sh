fa04ff5c53ab897bfb35cecb38c8183fc8445fac
Cristian Martinez <me@martinec.org>
2019-02-08 14:59:00 +0100

    [minor refactor] Update copyright date to 2019

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 # =============================================================================
 # Unitex/GramLab language resources installer
 # =============================================================================
-# Copyright (C) 2018 Université Paris-Est Marne-la-Vallée <unitex-devel@univ-mlv.fr>
+# Copyright (C) 2019 Université Paris-Est Marne-la-Vallée <unitex-devel@univ-mlv.fr>
 # 
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
